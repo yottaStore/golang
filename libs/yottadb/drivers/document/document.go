10f6b735908f9428f20f1f68112fd83a3d645f91
espoal <alberto.websci@gmail.com>
2022-09-05 19:21:55 +0200

    feat: collections mvp

--- libs/yottadb/drivers/document/document.go
+++ libs/yottadb/drivers/document/document.go
@@ -1,4 +1,4 @@
-package keyvalue
+package document
 
 import (
 	"bytes"
@@ -40,22 +40,6 @@ func (d Driver) Read(req yottadb.ReadRequest) (yottadb.ReadResponse, error) {
 	fmt.Println("Shards pool:", shards)
 	fmt.Println("Node picked: ", node)
 
-	// Issue read
-	values := map[string]interface{}{"Path": parsedRecord.RecordIdentifier}
-	json_data, err := json.Marshal(values)
-	if err != nil {
-		return resp, err
-	}
-	fmt.Println("Json data: ", string(json_data))
-	result, err := http.Post(node+"/yottafs/read", "application/json",
-		bytes.NewBuffer(json_data))
-	if err != nil {
-		return resp, err
-	}
-	body, err := io.ReadAll(result.Body)
-
-	resp.Data = body
-
 	// Return result
 	return resp, nil
 }
@@ -81,29 +65,6 @@ func (d Driver) Write(req yottadb.WriteRequest) (yottadb.WriteResponse, error) {
 	fmt.Println("Node tree: ", nodes)
 	fmt.Println("Shards pool:", shards)
 	fmt.Println("Node picked: ", node)
-
-	// Issue write
-	values := map[string]interface{}{"Path": parsedRecord.RecordIdentifier, "Data": req.Data}
-	json_data, err := json.Marshal(values)
-	if err != nil {
-		return resp, err
-	}
-
-	fmt.Println("Json data: ", string(json_data))
-	fmt.Println(node)
-	result, err := http.Post(node+"/yottafs/write", "application/json",
-		bytes.NewBuffer(json_data))
-	if err != nil {
-		return resp, err
-	}
-
-	buff, err := io.ReadAll(result.Body)
-	if err != nil {
-		return resp, err
-	}
-	if result.StatusCode != http.StatusOK {
-		return resp, errors.New(string(buff))
-	}
 
 	return resp, nil
 
@@ -138,33 +99,112 @@ func (d Driver) Append(req yottadb.WriteRequest) (yottadb.WriteResponse, error) 
 	fmt.Println("Shards pool:", shards)
 	fmt.Println("Node picked: ", node)
 
-	// Issue write
-	values := map[string]interface{}{"Path": parsedRecord.RecordIdentifier, "Data": req.Data}
-	json_data, err := json.Marshal(values)
-	if err != nil {
-		return resp, err
-	}
-
-	fmt.Println("Json data: ", string(json_data))
-	fmt.Println(node)
-	result, err := http.Post(node+"/yottafs/write", "application/json",
-		bytes.NewBuffer(json_data))
-	if err != nil {
-		return resp, err
-	}
-
-	buff, err := io.ReadAll(result.Body)
-	if err != nil {
-		return resp, err
-	}
-	if result.StatusCode != http.StatusOK {
-		return resp, errors.New(string(buff))
-	}
-
 	return resp, nil
 }
 
 func (d Driver) Delete(req yottadb.WriteRequest) error {
+
+	// Find nodes
+	opts := rendezvous.RendezvousOptions{
+		Replication: 1,
+		Sharding:    1,
+	}
+	shards, _, parsedRecord, err := d.Finder.FindRecord(req.Path, *d.Nodes, opts)
+	if err != nil {
+		return err
+	}
+
+	// TODO: pick all shards
+	node := shards[0]
+
+	// Issue write
+	values := map[string]interface{}{"Path": parsedRecord.RecordIdentifier}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Json data: ", string(json_data))
+	fmt.Println(node)
+	result, err := http.Post(node+"/yottafs/delete", "application/json",
+		bytes.NewBuffer(json_data))
+	if err != nil {
+		return err
+	}
+
+	buff, err := io.ReadAll(result.Body)
+	if err != nil {
+		return err
+	}
+	if result.StatusCode != http.StatusOK {
+		return errors.New(string(buff))
+	}
+
+	return nil
+}
+
+func (d Driver) GetCollection(req yottadb.ReadRequest) (yottadb.ReadResponse, error) {
+
+	var resp yottadb.ReadResponse
+	// Find nodes
+	// TODO: fix rendezvous
+	// TODO: improve API shape
+	// TODO: check collection for size
+	opts := rendezvous.RendezvousOptions{
+		Replication: 1,
+		Sharding:    1,
+	}
+	shards, nodes, parsedRecord, err := d.Finder.FindRecord(req.Path, *d.Nodes, opts)
+	if err != nil {
+		return resp, err
+	}
+
+	// TODO: pick a shard at random and verify others
+	node := shards[0]
+
+	fmt.Println("Record: ", parsedRecord)
+	fmt.Println("Node tree: ", nodes)
+	fmt.Println("Shards pool:", shards)
+	fmt.Println("Node picked: ", node)
+
+	// Return result
+	return resp, nil
+}
+
+func (d Driver) CreateCollection(req yottadb.WriteRequest) (yottadb.WriteResponse, error) {
+
+	var resp yottadb.WriteResponse
+
+	// Find nodes
+	opts := rendezvous.RendezvousOptions{
+		Replication: 1,
+		Sharding:    1,
+	}
+	shards, nodes, parsedRecord, err := d.Finder.FindRecord(req.Path, *d.Nodes, opts)
+	if err != nil {
+		return resp, err
+	}
+
+	// TODO: pick all shards
+	node := shards[0]
+
+	fmt.Println("Record: ", parsedRecord)
+	fmt.Println("Node tree: ", nodes)
+	fmt.Println("Shards pool:", shards)
+	fmt.Println("Node picked: ", node)
+
+	return resp, nil
+
+}
+
+func (d Driver) UpdateCollection(req yottadb.WriteRequest) (yottadb.WriteResponse, error) {
+
+	var resp yottadb.WriteResponse
+
+	return resp, errors.New("Method not implemented yet")
+}
+
+func (d Driver) DeleteCollection(req yottadb.WriteRequest) error {
 
 	// Find nodes
 	opts := rendezvous.RendezvousOptions{
